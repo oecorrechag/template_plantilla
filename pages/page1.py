b6bdcebd40bb3d083b1bbabaaf32503ded52b67a
oecorrechag <oecorrechag@unal.edu.co>
2022-12-25 09:53:57 -0500

    se cambio el archivo menu por callbackspage1

--- pages/page1.py
+++ pages/page1.py
@@ -1,6 +1,11 @@
 from dash import dcc, html
 
-from components import menu
+# from components import menu
+
+from components.CallbacksPage1 import Menu
+from components.CallbacksPage1 import Page1Box1
+from components.CallbacksPage1 import Page1Table
+from components.CallbacksPage1 import Page1Graph1
 
 layout1 = html.Div([
 
@@ -9,11 +14,15 @@ layout1 = html.Div([
     html.Br(),
     dcc.Link('Go to Home', href='/'), 
 
-    menu.layout_menu,
+    # menu.layout_menu,
+    Menu,
     html.H3('Page 1'),
 
-    menu.layout_menu1,
-    menu.layout_menu2,
-    menu.layout_menu3,
+    # menu.layout_menu1,
+    # menu.layout_menu2,
+    # menu.layout_menu3,
+    Page1Box1,
+    Page1Table, 
+    Page1Graph1,
 
 ])
